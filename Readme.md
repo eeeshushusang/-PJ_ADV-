2bfe32699c10e6b443fce0906967535e072c40f2
eeeshushusang <1693044702@qq.com>
2024-05-27 17:38:13 +0800

    Updated 1.1

--- Readme.md
+++ Readme.md
@@ -1,26 +1,106 @@
 # 视觉小说评价系统
-一. 表结构设计:
-    ADV信息表：包含ADV的ID、书名、制作社团、发行日期、发售平台、价格、评价(分数)等字段。
-    购买信息表：包含购买编码、ADV的ID、ADV的书名、购买日期。
-    客户信息表：包含购买编码、姓名、性别、评价(分数+字)、购买过的ADV等字段。
-    社团信息表：包含名字、制作的adv、创立时间、评分(根据作品加权评分)
-二. 基本功能要求:
-    ADV入库、信息修改和删除。
-    客户买ADV和评价ADV。
-    查询ADV信息和评价。
-    查询社团有哪些作品，社团评价
+## 一.目的和背景
+    视觉小说(ADV)是结合了视觉与听觉的第九艺术，在过去数十年内已造就无数经典。
+    目前日本有Adv评价网站 批评空间(https://erogamescape.org/~ap2/ero/toukei_kaiseki/)，西方有VNDB(https://vndb.org/).
+    而国内由于种种原因始终没有类似的网站，我决定不畏困难踏出一步，制作该平台让更多人了解到优秀的Adv作品。
+    据调查发现，这不仅是复旦有史以来第一个开创性的Adv平台项目，更是目前中国首创的Adv评价平台，具有极强的创新性。
 
-项目网页：
+    在该平台的基础层面，我已经实现了极其自由的查看各种信息，功能。并且为平台打造了极为美观的UI设计。
+## 二.数据库设计:
+### 1.表结构设计
+    ADV信息表：包含ADV的书名、外键制作社团、发行日期、发售平台、价格、评价(分数+评论)、分数。
+    评论信息表：包含外键用户评价(分数+评论)、外键ADV。
+    用户信息表：包含用户名、密码、性别、职业、头像。
+    社团信息表：包含名字、制作的adv、创立时间、评分(根据作品加权评分)、logo相片
+
+逻辑结构表格如下：
+![](12.png)
+![](13.png)
+### 2.概念结构设计
+E-R图如下：
+使用django-extension根据项目生成，右侧部分为主要models的设计。
+![](adv.png)
+
+### 3.物理结构设计
+在本项目中，我们选择使用关系型数据库管理系统(RDBMS)来存储和管理数据。根据每个表的主键和外键设计了相应的索引，以确保数据检索的效率和完整性。
+
+## 二. 系统设计与实现
+#### 基本功能要求:
+    1.用户注册和登录
+    2.用户添加Adv信息，Club信息。
+    3.用户评价ADV。
+    4.查看ADV信息。
+    5.查询社团信息
+#### 进阶功能：
+    1.自由地通过链接查看Adv的Club，Club的Adv，在Adv详情页转向评论等。
+    2.前端:
+        2.1看板娘
+        2.2为每个页面添加了独特的bgm.其中每个社团的bgm都是该社灵魂bgm，每个Adv同理。
+
+## 项目网页：
+主页如图，详细请点开网页，可以ip直连访问。
+![](index.png)
 http://49.235.184.91/index/
+
 http://www.adv.xn--6qq986b3xl/index/
+(域名报备没过....)
+
 项目github:
 https://github.com/eeeshushusang/-PJ_ADV-
 
-## 后端：
+## 后端：(工作量30%)
 galgame信息：
-wiki;CNGAL;DLsite;Steam;
+wiki; CNGAL; DLsite; Steam;
 
-## 前端：
+### 亮点逻辑：
+#### 用户评论后，自动层层更新相关分数。
+```python
+在保存评论时，自动调用相关Adv的保存函数，更新Adv的评分。
+# comments的save函数
+def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.adv.update_average_rate()
+Adv在保存时自动调用函数更新平均分。
+# Adv的更新和save函数
+def update_average_rate(self):
+        # 从 CompositeField 中提取 decimal_part 并计算平均值
+        comments = self.Comments_set.all()
+        total_score = sum([comment.comments.decimal_part for comment in comments])
+        count = comments.count()
+        self.rate = total_score / count if count > 0 else 0
+        self.save()
+        self.club.update_average_rate()  # 更新相关 Club 的评分
+继而，相关Club也会自动调用保存函数更新Adv平均分。
+# Club的save和更新函数
+def update_average_rate(self):
+        advs = self.adv_set.all()
+        total_score = sum([adv.rate for adv in advs])
+        count = advs.count()
+        self.rate = total_score / count if count > 0 else 0
+        self.save()
+```
+![](枕.png){width=50%}
+![](Club.png){width=50%}
+####  评论时，默认将使用的用户作为评论者
+```python
+相关代码：
+if comments_form.is_valid():
+            comment = comments_form.save(commit=False)
+            user_name = request.session.get("user_name")
+            if not user_name:
+                message = "用户未登录或未找到"
+                return render(
+                    request,
+                    "comments.html",
+                    {"comments_form": comments_form, "message": message},
+                )
+            user = get_object_or_404(User, name=user_name)
+            comment.user = user
+```
+![](Comments.png)
+注意，此处我们避免了使用我们定义的User来替代Django默认Usser的做法，那样会过于复杂。
+
+## 前端：(工作量50%)
 live2d最简单的抄法：
 https://github.com/stevenjoezhang/live2d-widget
 登陆,注册，上传信息等页面样式参考：
@@ -29,8 +109,8 @@ https://www.bilibili.com/video/BV1X3411y7of/?spm_id_from=333.880.my_history.page
 https://www.bilibili.com/video/BV1wb4y1a7uQ/?spm_id_from=333.880.my_history.page.click
 
 ### 图片：
--    全局背景图：Anmi
--    部分网页框内背景：Weri
+-    全局背景：Anmi https://www.pixiv.net/users/212801
+-    部分网页框内背景：Weri https://www.pixiv.net/users/20728711
 
 ### 音乐：
 -    首页，上传信息页： MANYO,三輪学 - 黑猫さんの背伸び
@@ -48,11 +128,15 @@ https://www.bilibili.com/video/BV1wb4y1a7uQ/?spm_id_from=333.880.my_history.page
         Summer Pockets:麻枝准 - Sea, You & Me
         Clannad:麻枝准 - 同じ高みへ
 
-### 部署上线：   
-    腾讯云：服务器(学生折扣)10r/月;域名：首年10元(便宜的中文域名)
-    使用nginx绕过域名报备进行上线。麻烦的一批。
+### 部署上线：(工作量20%)   
+腾讯云：服务器(学生折扣)10r/月;
+域名：首年10元(便宜的中文域名)
+使用nginx绕过域名报备进行上线，非常麻烦，实现了可IP直连访问。
+遗憾的是，域名审批终究没有通过，原因是使用中文域名，无法添加CNAME解析，SSL审核未通过。
+这也告诉我们，做个人网页还是选择海外服务器比较好，可以免去报备域名的工作。
 
 ### 后续跟进计划：
-- live2d，本打算从BA中扣一个乐队爱丽的小人模型下来，结果发现不会用相关库，不了了之。
-- 音乐未实现，同一曲目在不同网页播放时，继承上个网页的播放进度
+- live2d：本打算从BA中扣一个乐队爱丽的小人模型下来，结果发现不会用相关软件，不了了之。
+- 音乐：实现同一曲目在不同网页播放时，继承上个网页的播放进度
 - 鼠标特效可以做的更好看点
+- 修改bgm的model逻辑，将bgm作为Adv的属性添加，从而可以更自然地进行处理。
--- adv/views.py
+++ adv/views.py
@@ -11,7 +11,7 @@ from . import models
 import hashlib
 
 
-def hash_code(s, salt='pj_adv'):
+def hash_code(s, salt="pj_adv"):
     h = hashlib.sha256()
     s += salt
     h.update(s.encode())
@@ -25,66 +25,66 @@ class AdvViewSet(viewsets.ModelViewSet):
 
 def index(request):
     pass
-    return render(request, 'index.html')
+    return render(request, "index.html")
 
 
 def login(request):
-    if request.session.get('is_login', None):  # 不允许重复登录
-        return redirect('/index/')
+    if request.session.get("is_login", None):  # 不允许重复登录
+        return redirect("/index/")
 
-    if request.method == 'POST':
+    if request.method == "POST":
         login_form = LoginForm(request.POST)
-        message = '请检查填写的内容！'
+        message = "请检查填写的内容！"
         if login_form.is_valid():
-            username = login_form.cleaned_data.get('username')
-            password = login_form.cleaned_data.get('password')
+            username = login_form.cleaned_data.get("username")
+            password = login_form.cleaned_data.get("password")
             try:
                 user = User.objects.get(name=username)
             except User.DoesNotExist:
-                message = '用户不存在！'
-                return render(request, 'login.html', locals())
+                message = "用户不存在！"
+                return render(request, "login.html", locals())
 
             if user.password == password:
-                request.session['is_login'] = True
-                request.session['user_id'] = user.id
-                request.session['user_name'] = user.name
+                request.session["is_login"] = True
+                request.session["user_id"] = user.id
+                request.session["user_name"] = user.name
 
                 # 获取 next 参数，如果没有则重定向到首页
-                next_url = request.GET.get('next', '/index/')
+                next_url = request.GET.get("next", "/index/")
                 return redirect(next_url)
             else:
-                message = '密码不正确！'
-                return render(request, 'login.html', locals())
+                message = "密码不正确！"
+                return render(request, "login.html", locals())
         else:
-            return render(request, 'login.html', locals())
+            return render(request, "login.html", locals())
 
     login_form = LoginForm()
-    return render(request, 'login.html', locals())
+    return render(request, "login.html", locals())
 
 
 def register(request):
-    if request.session.get('is_login', None):
-        return redirect('/index/')
+    if request.session.get("is_login", None):
+        return redirect("/index/")
 
-    if request.method == 'POST':
+    if request.method == "POST":
         register_form = forms.RegisterForm(request.POST, request.FILES)
         message = "请检查填写的内容！"
         if register_form.is_valid():
-            username = register_form.cleaned_data.get('username')
-            password1 = register_form.cleaned_data.get('password1')
-            password2 = register_form.cleaned_data.get('password2')
-            sex = register_form.cleaned_data.get('sex')
-            job = register_form.cleaned_data.get('job')
-            photo = register_form.cleaned_data['photo']
+            username = register_form.cleaned_data.get("username")
+            password1 = register_form.cleaned_data.get("password1")
+            password2 = register_form.cleaned_data.get("password2")
+            sex = register_form.cleaned_data.get("sex")
+            job = register_form.cleaned_data.get("job")
+            photo = register_form.cleaned_data["photo"]
 
             if password1 != password2:
-                message = '两次输入的密码不同！'
-                return render(request, 'register.html', locals())
+                message = "两次输入的密码不同！"
+                return render(request, "register.html", locals())
             else:
                 same_name_user = models.User.objects.filter(name=username)
                 if same_name_user:
-                    message = '用户名已经存在'
-                    return render(request, 'register.html', locals())
+                    message = "用户名已经存在"
+                    return render(request, "register.html", locals())
                 new_user = models.User()
                 new_user.name = username
                 new_user.password = hash_code(password1)
@@ -92,65 +92,64 @@ def register(request):
                 new_user.sex = sex
                 new_user.job = job  # 确保保存job字段
                 new_user.save()
-                return redirect('/login/')  # 注册成功，重定向到登录页面
+                return redirect("/login/")  # 注册成功，重定向到登录页面
 
         else:
-            return render(request, 'register.html', locals())
+            return render(request, "register.html", locals())
     register_form = forms.RegisterForm()
-    return render(request, 'register.html', locals())
+    return render(request, "register.html", locals())
 
 
 def logout(request):
-    if not request.session.get('is_login', None):
-        return redirect('/login/')
+    if not request.session.get("is_login", None):
+        return redirect("/login/")
 
     request.session.flush()
     # del request.session['is_login']
     return redirect("/login/")
 
 
-# TODO
 def update_club(request):  # 上传某club信息
-    if request.method == 'POST':
-        club_update_form = forms.ClubUpdateForm(request.POST,request.FILES)
+    if request.method == "POST":
+        club_update_form = forms.ClubUpdateForm(request.POST, request.FILES)
         message = "请检查填写的内容！"
         if club_update_form.is_valid():
-            username = club_update_form.cleaned_data.get('username')
-            pub_date = club_update_form.cleaned_data.get('pub_date')
-            photo = club_update_form.cleaned_data['photo']
+            username = club_update_form.cleaned_data.get("username")
+            pub_date = club_update_form.cleaned_data.get("pub_date")
+            photo = club_update_form.cleaned_data["photo"]
 
             same_name_user = models.Club.objects.filter(name=username)
             if same_name_user:
-                message = '该社团名已经存在，请选择其他名称'
-                return render(request, 'update_club.html', locals())
+                message = "该社团名已经存在，请选择其他名称"
+                return render(request, "update_club.html", locals())
             new_Club = models.Club()
             new_Club.name = username
             new_Club.pub_date = pub_date
             new_Club.photo = photo
             new_Club.save()
-            return redirect('/index/')  # 上传成功
+            return redirect("/index/")  # 上传成功
         else:
-            return render(request, 'update_club.html', locals())
+            return render(request, "update_club.html", locals())
     club_update_form = forms.ClubUpdateForm()
-    return render(request, 'update_club.html', locals())
+    return render(request, "update_club.html", locals())
 
 
 def update_adv(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         adv_form = forms.AdvUpdateForm(request.POST, request.FILES)
         message = "请检查填写的内容！"
         if adv_form.is_valid():
-            club = adv_form.cleaned_data.get('club')
-            title = adv_form.cleaned_data.get('title')
-            pub_date = adv_form.cleaned_data.get('pub_date')
-            price = adv_form.cleaned_data.get('price')
-            platform = adv_form.cleaned_data.get('platform')
-            photo = adv_form.cleaned_data['photo']
+            club = adv_form.cleaned_data.get("club")
+            title = adv_form.cleaned_data.get("title")
+            pub_date = adv_form.cleaned_data.get("pub_date")
+            price = adv_form.cleaned_data.get("price")
+            platform = adv_form.cleaned_data.get("platform")
+            photo = adv_form.cleaned_data["photo"]
 
             same_title_adv = models.Adv.objects.filter(title=title)
             if same_title_adv.exists():
-                message = '该ADV名已经存在，请选择其他名称'
-                return render(request, 'update_adv.html', locals())
+                message = "该ADV名已经存在，请选择其他名称"
+                return render(request, "update_adv.html", locals())
 
             new_adv = models.Adv(
                 club=club,
@@ -158,53 +157,62 @@ def update_adv(request):
                 pub_date=pub_date,
                 price=price,
                 platform=platform,
-                photo=photo
+                photo=photo,
             )
             new_adv.save()
-            return redirect('/index/')  # 上传成功
+            return redirect("/index/")  # 上传成功
 
         else:
-            return render(request, 'update_adv.html', locals())
+            return render(request, "update_adv.html", locals())
 
     adv_form = forms.AdvUpdateForm()
-    return render(request, 'update_adv.html', locals())
+    return render(request, "update_adv.html", locals())
 
 
-# TODO
 def comments(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         comments_form = CommentsForm(request.POST, request.FILES)
         if comments_form.is_valid():
             comment = comments_form.save(commit=False)
-            user_name = request.session.get('user_name')
+            user_name = request.session.get("user_name")
             if not user_name:
                 message = "用户未登录或未找到"
-                return render(request, 'comments.html', {'comments_form': comments_form, 'message': message})
+                return render(
+                    request,
+                    "comments.html",
+                    {"comments_form": comments_form, "message": message},
+                )
             user = get_object_or_404(User, name=user_name)
             comment.user = user
             composite_value = CompositeValue(
-                decimal_part=comments_form.cleaned_data['decimal_part'],
-                char_part=comments_form.cleaned_data['char_part']
+                decimal_part=comments_form.cleaned_data["decimal_part"],
+                char_part=comments_form.cleaned_data["char_part"],
             )
             comment.comments = composite_value
             comment.save()  # 调用 Comments 模型的 save 方法
-            return redirect('/index/')
+            return redirect("/index/")
         else:
             message = "表单验证失败，请检查填写内容。"
-            return render(request, 'comments.html', {'comments_form': comments_form, 'message': message})
+            return render(
+                request,
+                "comments.html",
+                {"comments_form": comments_form, "message": message},
+            )
     else:
         comments_form = CommentsForm()
-        return render(request, 'comments.html', {'comments_form': comments_form})
+        return render(request, "comments.html", {"comments_form": comments_form})
 
 
 def adv_list(request):
     advs = Adv.objects.all()
-    return render(request, 'adv_list.html', {'advs': advs})
+    return render(request, "adv_list.html", {"advs": advs})
 
 
 def adv_detail(request, adv_title):
     adv = get_object_or_404(Adv, title=adv_title)
-    comments = Comments.objects.filter(adv=adv).select_related('user')  # 确保包括关联的用户信息
+    comments = Comments.objects.filter(adv=adv).select_related(
+        "user"
+    )  # 确保包括关联的用户信息
     # 根据 ADV 的标题设置对应的 BGM
     bgm_files = {
         "高考恋爱一百天": "adv/bgm/Days - Nuit Silencieuse.mp3",
@@ -215,23 +223,17 @@ def adv_detail(request, adv_title):
         "兰斯10 决战": "adv/bgm/アリスソフト - the end.mp3",
         "White Album": "adv/bgm/石川真也 - FILL YOU.mp3",
         "White Album2": "adv/bgm/石川真也 - あの頃のように.mp3",
-        "Clannad": "adv/bgm/麻枝准 - 同じ高みへ.mp3"
-
+        "Clannad": "adv/bgm/麻枝准 - 同じ高みへ.mp3",
     }
     bgm_file = bgm_files.get(adv.title, "默认 BGM 文件路径")
 
-    context = {
-        'adv': adv,
-        'comments': comments,
-        'bgm': bgm_file
-    }
-    return render(request, 'adv_detail.html', context)
-
+    context = {"adv": adv, "comments": comments, "bgm": bgm_file}
+    return render(request, "adv_detail.html", context)
 
 
 def club_list(request):
     clubs = Club.objects.all()
-    return render(request, 'club_list.html', {'clubs': clubs})
+    return render(request, "club_list.html", {"clubs": clubs})
 
 
 def club_detail(request, club_name):
@@ -248,10 +250,5 @@ def club_detail(request, club_name):
     }
     bgm_file = bgm_files.get(club.name, "默认 BGM 文件路径")
 
-    context = {
-        'club': club,
-        'advs': advs,
-        'bgm': bgm_file
-    }
-    return render(request, 'club_detail.html', context)
-
+    context = {"club": club, "advs": advs, "bgm": bgm_file}
+    return render(request, "club_detail.html", context)
